05d1c1a31b755e830df846c82fd39f98ba959ffb
nazifa1505 <142781203+nazifa1505@users.noreply.github.com>
2023-09-06 18:37:52 +0200

    Update streamlit_app.py
    syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,8 +55,3 @@ try:
     
     add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
     my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-  
-  
-  
-
-
