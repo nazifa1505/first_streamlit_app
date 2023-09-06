304650015c0c9d735dc94c64d36bfa4555d22430
nazifa1505 <142781203+nazifa1505@users.noreply.github.com>
2023-09-06 14:08:44 +0200

    Update streamlit_app.py
    had a paranthesis syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response).json()) 
+streamlit.text(fruityvice_response.json()) 
 
 # normalizes the version of the json responses
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
