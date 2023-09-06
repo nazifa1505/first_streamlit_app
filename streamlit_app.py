a43387632b7a0325c68976d3c47e5fd6862aac6f
nazifa1505 <142781203+nazifa1505@users.noreply.github.com>
2023-09-06 19:49:47 +0200

    Update streamlit_app.py
    syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,14 +46,14 @@ try:
 
   #import snowflake.connector
   streamlit.header("The fruit load list contains:")
-  def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
+    def get_fruit_load_list():
+      with my_cnx.cursor() as my_cur:
          my_cur.execute("SELECT * from fruit_load_list")
          return my_cur.fetchall()
-  if streamlit.button('Get Fruit Load List'):
-      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-      my_data_rows = get_fruit_load_list()
-      streamlit.dataframe(my_data_rows)
+    if streamlit.button('Get Fruit Load List'):
+        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+        my_data_rows = get_fruit_load_list()
+        streamlit.dataframe(my_data_rows)
     def insert_row_snowflake(new_fruit):
          with my_cnx.cursor() as my_cur:
               my_cur.execute("insert into fruit_load_list values ('from streamlit')")
