bf2ad5c6346724b615774dec727f2e7617a4b71b
nazifa1505 <142781203+nazifa1505@users.noreply.github.com>
2023-09-06 18:36:36 +0200

    Update streamlit_app.py
    syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,19 +44,19 @@ try:
 
   #streamlit.stop()
 
-#import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
-add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
-
-
+  #import snowflake.connector
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_cur = my_cnx.cursor()
+  my_cur.execute("SELECT * from fruit_load_list")
+  my_data_rows = my_cur.fetchall()
+  
+  streamlit.header("The fruit load list contains:")
+  streamlit.dataframe(my_data_rows)
+  
+  add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
+  my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+  
+  
+  
 
 
